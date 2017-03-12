ad58162571502ad690060100c96de09359f8292e
Keeley Hammond <keeleymhammond@gmail.com>
2017-03-11 22:43:33 -0800

    Favicon added and rough jQuery added, need to populate table and pull in charts

--- js/main.js
+++ js/main.js
@@ -14,9 +14,21 @@ function Runner(data) {
   this.athleteActivity = data.athleteActivity;
 }
 
+// populate a table for now with my sample three runners
+Runner.prototype.toHtml() = function() {
+  var $newRunner = $('article');
+  $('#runners tr').not(':first').not(':last').remove();
+  var html = '';
+  for (var i = 0; i < data.length; i++)
+    html += '<tr><td>' + data[i].FirstName +
+      '</td><td>' + data[i].Age + '</td></tr>';
+  $('#thetable tr').first().after(html);
+}
+
 data.forEach(function(i) {
   runners.push(new Runner(i));
 });
 
-
-
+runners.forEach(function(runner) {
+  $('#runners').append(runner.toHtml());
+});
